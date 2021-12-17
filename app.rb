d8f48f1f8f56272cd1354f736324b84e560afc0a
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-17 08:32:06 -0800

    add routing structure for words in app.rb

--- app.rb
+++ app.rb
@@ -4,3 +4,36 @@ require('./lib/word')
 require('pry')
 also_reload('lib/**/*.rb')
 require('./lib/definition')
+
+get('/') do
+
+end
+
+get('/words') do
+  
+end
+
+get('words/new') do
+
+end
+
+get('words/:id') do
+  
+end
+
+post('/words') do
+
+end
+
+get('/albums/:id/edit') do
+
+end
+
+patch('/albums/:id') do
+
+end
+
+delete('/albums/:id') do
+
+end
+
