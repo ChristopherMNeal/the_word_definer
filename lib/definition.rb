78a31523b53a77de015c576e9a9793539e5c45db
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-17 14:19:03 -0800

    add structure for definition logic and tests

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,1 +1,15 @@
 require ('pry')
+
+class definition
+  attr_reader :id
+  attr_accessor :name, :word_id
+
+  @@definitions = {}
+  @@total_rows = 0
+
+  def initialization(attributes)
+    @definition = attributes.fetch(:defintion)
+    @id = attributes.fetch(:id) || @@total_rows += 1
+  end
+
+end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -2,3 +2,24 @@ require 'rspec'
 require 'word'
 require 'definition'
 require 'pry'
+
+describe '#Defintion' do
+
+  before(:each) do
+  end
+
+  describe('') do
+    it("") do
+    end
+  end
+
+  describe('') do
+    it("") do
+    end
+  end
+
+  describe('') do
+    it("") do
+    end
+  end
+end
