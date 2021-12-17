c9e2bca397209c59829a2105babe7696ed7aca41
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-17 09:37:47 -0800

    pass test for Word#save, add '==' overwrite method

--- lib/word.rb
+++ lib/word.rb
@@ -1,18 +1,27 @@
 require("pry")
 
 class Word
-  attr_reader :id, :name
+  attr_reader :id, :word
 
   @@words = {}
   @@total_rows = 0
 
   def initialize(attributes)
     @word = attributes.fetch(:word)
-    @id = attributes.fetch(:id) || @@total_rows += 1
+    # @id = attributes.fetch(:id) || @@total_rows += 1
+    @id = attributes[:id] || @@total_rows += 1
   end
 
   def self.all()
     @@words.values()
+  end
+
+  def save
+    @@words[self.id] = Word.new({word: word, id: id})
+  end
+
+  def ==(word_to_compare)
+    self.word() == word_to_compare.word()
   end
 
   def self.find(word)
