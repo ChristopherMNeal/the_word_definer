79b76818494f59b45fa1fa5a29a2c97d49c48767
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-17 14:28:00 -0800

    pass first test for Definition.all

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,6 +1,6 @@
 require ('pry')
 
-class definition
+class Definition
   attr_reader :id
   attr_accessor :name, :word_id
 
@@ -10,6 +10,10 @@ class definition
   def initialization(attributes)
     @definition = attributes.fetch(:defintion)
     @id = attributes.fetch(:id) || @@total_rows += 1
+  end
+
+  def self.all()
+    @@definitions.values()
   end
 
 end
