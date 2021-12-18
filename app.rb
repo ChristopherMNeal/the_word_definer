7fa95eed18534ecda803b53befa1d03dda6e0e78
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-18 14:42:40 -0800

    BUGFIX: correct typo in require

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
 require('sinatra')
 require('sinatra/reloader')
 require('./lib/word')
+require('./lib/definition')
 require('pry')
 also_reload('lib/**/*.rb')
-require('./lib/defi')
 
 get('/') do
   @words = Word.all
