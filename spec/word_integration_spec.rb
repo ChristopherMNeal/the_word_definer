039535ba9f851faf3892677b6dddca302849429b
Christopher Neal <christopher.m.neal@gmail.com>
2021-12-25 18:52:59 -0800

    add and pass fourth, fifth, sixth integration tests

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -24,13 +24,49 @@ describe('', {:type => :feature}) do
 end
 
 describe('', {:type => :feature}) do
-  it('') do
+  it('updates a word') do
     visit('/words')
     click_on('Bamboozle')
     click_on("Edit 'Bamboozle'")
     fill_in('word', :with => 'Hornswoggle')
     click_on('Update')
     expect(page).to have_content('Hornswoggle')
+  end
+end
+
+describe('', {:type => :feature}) do
+  it('deletes a word') do
+    visit('/words')
+    click_on('Hornswoggle')
+    click_on("Edit 'Hornswoggle'")
+    click_on("Delete word")
+    expect(page).to have_no_content('Hornswoggle')
+  end
+end
+
+describe('', {:type => :feature}) do
+  it('adds a definition then changes the definition') do
+    visit('/words')
+    click_on('Add a new word')
+    fill_in('word_to_add', :with => 'Hoodwink')
+    click_on('Go!')
+    click_on('Hoodwink')
+    fill_in('defi_name', :with => 'to fool someone')
+    click_on('Add definition')
+    click_on('to fool someone')
+    fill_in('name', :with => 'to con someone')
+    click_on('Update definition')
+    expect(page).to have_content('to con someone')
+  end
+end
+
+describe('', {:type => :feature}) do
+  it('deletes a definition') do
+    visit('/words')
+    click_on('Hornswoggle')
+    click_on("Edit 'Hornswoggle'")
+    click_on("Delete word")
+    expect(page).to have_no_content('Hornswoggle')
   end
 end
 
